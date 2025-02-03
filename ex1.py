6ca00f8a6c47d1c4824e413973cd023b1cc0e705
danil doe <danildoe@example.com>
2025-02-03 17:20:55 +0300

    ex.1

--- ex1.py
+++ ex1.py
@@ -1,2 +1,15 @@
-print('Hello it park')
-
+def sum_list(a):
+    s = 0
+    s1 = 0
+    for x in a:
+        if type(x) == int:
+            s += x
+        elif isinstance(x, str):
+            s1 += len(x)
+        elif type(x) == list:
+          ss, ss1 = sum_list(x)
+          s += ss
+          s1 += ss1
+    return s, s1
+a = [1,3,5,7,[12,3, 'q'],'snds']
+print(sum_list(a))
